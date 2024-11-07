2f5400ca7ec2a98e5a840e0b9bbb59d44c24fbd8
YuCheol <160010579+Yu-Cheol@users.noreply.github.com>
2024-11-07 13:49:55 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        Long i = scan.nextLong(), j = scan.nextLong(), k = scan.nextLong();
+        System.out.println(i + j + k);
+
+    }
+}
