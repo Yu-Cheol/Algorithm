bd46e5b7cb73af61aca2f435fa1ff613385ff008
YuCheol <160010579+Yu-Cheol@users.noreply.github.com>
2024-11-07 11:39:43 +0900

    [Bronze V] Title: A×B, Time: 96 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        int j = scan.nextInt();
+        
+        int res = i * j;
+        
+        System.out.println(res);
+    }
+}
