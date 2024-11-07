5d3ba93078e809728be2cd3051b86ee76871d3b1
YuCheol <160010579+Yu-Cheol@users.noreply.github.com>
2024-11-07 13:05:42 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        double i = scan.nextDouble();
+        double j = scan.nextDouble();
+        
+        double res = i / j;
+        
+        System.out.println(res);
+        
+    }
+}
