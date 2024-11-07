41be516abd3fe85ca90639db9d425eeb498edbee
YuCheol <160010579+Yu-Cheol@users.noreply.github.com>
2024-11-07 11:36:24 +0900

    [Bronze V] Title: A-B, Time: 96 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        int j = scan.nextInt();
+        
+        int res = i - j;
+        
+        System.out.println(res);
+    }
+}
