d548444d7f36cb4b01b2ba3cd1c41f121b9ef907
YuCheol <160010579+Yu-Cheol@users.noreply.github.com>
2024-11-07 13:13:06 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        int j = scan.nextInt();
+        
+        System.out.println(i + j);
+        System.out.println(i - j);
+        System.out.println(i * j);
+        System.out.println(i / j);
+        System.out.println(i % j);
+        
+    }
+}
